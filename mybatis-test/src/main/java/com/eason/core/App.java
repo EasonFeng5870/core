2904f3d3a90a4ad07a93993d753c3e42c095adfc
fengyingsheng <fengyingsheng@dangdang.com>
2017-09-05 09:14:56 +0800

    --

--- mybatis-test/src/main/java/com/eason/core/App.java
+++ mybatis-test/src/main/java/com/eason/core/App.java
@@ -1,5 +1,6 @@
 package com.eason.core;
 
+import org.apache.ibatis.session.Configuration;
 import org.apache.ibatis.type.TypeAliasRegistry;
 
 /**
@@ -11,6 +12,7 @@ public class App
     public static void main( String[] args )
     {
         TypeAliasRegistry registry;
+        Configuration configuration;
         System.out.println( "Hello World!" );
     }
 }
